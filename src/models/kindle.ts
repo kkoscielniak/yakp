5f45fd685ec7e8bddd5da48c545069a7c766375e
Krystian Kościelniak <krystian@koscielniak.pro>
2022-07-24 18:36:43 +0200

    feat: Add parsing functionality

--- src/models/kindle.ts
+++ src/models/kindle.ts
@@ -1,4 +1,11 @@
 export interface KindleNotebook {
   title: string;
   authors: string; // TODO: Make it array of strings
+  highlights: Highlight[];
+}
+
+export interface Highlight {
+  color: string | null;
+  location: number;
+  text: string;
 }
--- src/services/HtmlParser/HtmlParser.ts
+++ src/services/HtmlParser/HtmlParser.ts
@@ -1,9 +1,14 @@
 import { parse, HTMLElement } from 'node-html-parser';
-import { KindleNotebook } from '../../models/kindle';
+import { Highlight, KindleNotebook } from '../../models/kindle';
 
 enum QuerySelectorsEnum {
   TITLE = 'div.bookTitle',
   AUTHORS = 'div.authors',
+}
+
+enum ClassesEnum {
+  NOTE_HEADING = 'noteHeading',
+  NOTE_TEXT = 'noteText',
 }
 
 export default class HtmlParser {
@@ -15,18 +20,59 @@ export default class HtmlParser {
 
   private parseMetadata(querySelector: QuerySelectorsEnum): string {
     return this.#html
-      .querySelector(querySelector)
-      ?.removeWhitespace()
-      .textContent.trimStart()
-      .trimEnd()!;
+      .querySelector(querySelector)!
+      .removeWhitespace()
+      .textContent.trim();
+  }
+
+  private getColorFromDiv(div: HTMLElement): string | null {
+    const colorSpan = div.querySelector('span');
+
+    if (!colorSpan) {
+      return null;
+    }
+
+    return colorSpan.textContent.trim();
+  }
+
+  private getLocationFromDiv(div: HTMLElement): number {
+    return Number(
+      div.removeWhitespace().textContent.trim().match(/[0-9]/g)?.join('')
+    );
+  }
+
+  private getNoteFromDiv(div: HTMLElement): string {
+    return div.removeWhitespace().structuredText.trim();
   }
 
   parseHtml(): KindleNotebook {
-    let result: KindleNotebook = {
+    let notebook: KindleNotebook = {
       title: this.parseMetadata(QuerySelectorsEnum.TITLE),
       authors: this.parseMetadata(QuerySelectorsEnum.AUTHORS),
+      highlights: [],
     };
 
-    return result;
+    const divsToQuery = Object.values(ClassesEnum)
+      .map((divClass) => `div.${divClass}`)
+      .join(', ');
+
+    const divs = this.#html.querySelectorAll(divsToQuery);
+
+    let currentHighlight: Highlight | undefined;
+
+    divs.forEach((div) => {
+      if (div.classNames.includes(ClassesEnum.NOTE_HEADING)) {
+        currentHighlight = {
+          color: this.getColorFromDiv(div),
+          location: this.getLocationFromDiv(div),
+          text: '',
+        };
+      } else if (div.classNames.includes(ClassesEnum.NOTE_TEXT)) {
+        currentHighlight!.text = this.getNoteFromDiv(div);
+        notebook.highlights.push(currentHighlight!);
+      }
+    });
+
+    return notebook;
   }
 }
